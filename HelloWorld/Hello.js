89165b5b21bfcccbc530b4370958cb7901b7c498
Ridham1010 <ridhamshah1002@gmail.com>
2025-10-27 18:00:24 +0530

    Learnt module os

--- HelloWorld/Hello.js
+++ HelloWorld/Hello.js
@@ -1,4 +1,5 @@
 const math = require('./math');
-
-console.log("Hello, World!");
-console.log("Sum of 5 and 3 is: " + math(5, 3));
+const os= require("os");
+// console.log("Hello, World!");
+// console.log("Sum of 5 and 3 is: " + math(5, 3));
+console.log("cpu info is: " + os.cpus().length);
